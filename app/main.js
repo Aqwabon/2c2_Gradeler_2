ae214b62780b12e69a5805ae6e653e94f181e862
Aqwabon <ethanlang69@icloud.com>
2025-12-04 11:43:56 +0100

    je sais pas trop quoi ecrire

--- app/main.js
+++ app/main.js
@@ -1,19 +1,29 @@
 import './style.css';
 import {Map, View} from 'ol';
+import { ImageWMS } from 'ol/source';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import ImageLayer from 'ol/layer/Image';
+import OSM from 'ol/source/OSM';  
 
+const layers = [
+  new TileLayer({
+    source: new OSM(),
+  }),
+  new ImageLayer({
+    extent: [-13884991, 2870341, -7455066, 6338219],
+    source: new ImageWMS({
+      url: 'https://ahocevar.com/geoserver/wms',
+      params: {'LAYERS': 'topp:states'},
+      ratio: 1,
+      serverType: 'geoserver',
+    }),
+  }),
+];
 const map = new Map({
+  layers: layers,
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    center: [-10997148, 4569099],
+    zoom: 4,
+  }),
 });
-
-console.log("Bonjour hehe");
